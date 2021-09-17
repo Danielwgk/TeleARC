d795867f45166177ef0c3e9491787ea188d3477d
Danielwgk <danielwgk@gmail.com>
2021-09-16 21:52:48 -0500

    Update conf_models.py

--- conf_models.py
+++ conf_models.py
@@ -81,7 +81,7 @@ class Joystick(object):
         return Ch(lambda evts: 1. if self._joy.get_button(button) else -1.)
 
     def toggle(self, toggle_button):
-        if self.toggle_value[toggle_button] == None:
+        if self.toggle_value.get(toggle_button) == None:
             self.toggle_value[toggle_button] = True
         else:
             self.toggle_value[toggle_button] != self.toggle_value[toggle_button]
